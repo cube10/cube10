7178707b9f846f7691595bfc50877a9bd19484a2
haanj <joshua.haan@ymail.com>
2016-05-02 11:40:33 -0700

    added cross-origin authorization

--- resource-server/index.js
+++ resource-server/index.js
@@ -7,7 +7,16 @@ mongoose.connect(DB)
 let express = require('express')
 let app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Headers', 'Overwrite, Destination, Content-Type, Depth, User-Agent, Translate, Range, Content-Range, Timeout, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Location, Lock-Token, If')
+  res.header('Access-Control-Allow-Methods', 'ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL')
+  next()
+})
+
 let publicRouter = express.Router()
+
 
 require(__dirname + '/routes/login_route')(publicRouter)
 require(__dirname + '/routes/story_route')(publicRouter)
