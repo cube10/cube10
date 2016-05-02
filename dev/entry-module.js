c86065d33b75189a9040e217ad718bbd1eb562f9
Sam Gruse <sgruse89@gmail.com>
2016-05-02 12:04:26 -0700

    Set up gulpfile/test folders/ packages

---
+++ dev/entry-module.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Put any modules needed into the dependencies for the 'App' module.
+
+const angular = require('angular');
+
+(function () {
+  angular.module('App', ['MainModule'])
+})();
