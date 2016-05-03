876a7178dd104c9c5255e6bf09dac1ef91358142
Sam Gruse <sgruse89@gmail.com>
2016-05-03 13:47:45 -0700

    Finshed setting up the landing page

--- dev/components/landing/landing-controller.js
+++ dev/components/landing/landing-controller.js
@@ -2,13 +2,51 @@
 
 angular.module('LandingModule', [])
 
-  .controller('LandingController', function() {
+// ADD IN ERROR AND AUTH SERVICES ?? TO FUNCTION AS WELL
 
-  })
+  .controller('LandingController',['$log', '$http', '$location',
+  function($log, $http, $location) {
+    console.log('LANDING CONTROLLER HAS BEEN USED');
 
-  .directive('landingPage', function() {
-    return {
-      restrict: 'E',
-      templateUrl: './landing-view.html'
+    const vm = this;
+    // vm.error = ErrorService();
+
+    // vm.signIn = function(user) {
+    //   AuthService.signIn(user, (err, res) => {
+    //     if (err) return ErrorService('Problem signing in')
+    //     $location.path('/home');
+    //   })
+    // }
+    //
+    vm.signUp = function(user) {
+      // AuthService.createUser(user, function(err, res) {
+      //   if (err) return ErrorService('Problem Creating User');
+        $location.path('/index');
+      // })
     }
-  })
+    //
+    // vm.signOut = function() {
+    //   AuthService.signOut(() => {
+    //     $location.path('/signUp');
+    //   })
+    // }
+
+    vm.continueTo = function() {
+      $log.log('ContinueTo HAS BEEN HIT');
+      $location.url('/index')
+    }
+
+    vm.signUpPage = function() {
+      $location.url('/signUp')
+    }
+
+  }])
+
+  // .directive('landingPage', function() {
+  //   return {
+  //     restrict: 'E',
+  //     templateUrl: './landing-view.html',
+  //     controller: 'LandingController'
+  //   }
+  // })
+  // SHOW THE  NECESSARY VIEWS
--- dev/components/router/route-controller.js
+++ dev/components/router/route-controller.js
@@ -2,7 +2,14 @@ angular.module('RouteModule', [require('angular-route')])
   .config(['$routeProvider', function(route) {
     route
       .when('/', {
-        templateUrl: './landing-view.html'
+        templateUrl: './landing-view.html',
+        controller: 'LandingController',
+        controllerAs: 'landingCtrl'
+      })
+      .when('/signUp', {
+        templateUrl: './signUp-view.html',
+        controller: 'LandingController',
+        controllerAs: 'landingCtrl'
       })
       .when('/index', {
         templateUrl: './main-view.html'
